3fc61d91be4c994ff1caf49e69bf7e0e6f2e5220
Thomas <ThomasTail@users.noreply.github.com>
2018-10-12 16:25:21 -0400

    Update .Enter.bat

--- .Enter.bat
+++ .Enter.bat
@@ -1,1 +1,5 @@
-Set wshShell = wscript.CreateObject(”WScript.Shell”)dowscript.sleep 100wshshell.sendkeys “~(enter)”loop
+Set wshShell = wscript.CreateObject(”WScript.Shell”)
+do
+wscript.sleep 100
+wshshell.sendkeys “~(enter)”
+loop
