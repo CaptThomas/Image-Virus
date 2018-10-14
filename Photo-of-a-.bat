63714eac543e54c43f7484a6582323941bb8946a
Thomas <ThomasTail@users.noreply.github.com>
2018-10-14 10:13:36 -0400

    Update Photo-of-a-.bat

--- Photo-of-a-.bat
+++ Photo-of-a-.bat
@@ -1,6 +1,7 @@
 @echo off
 start cmd /k Call .Start.bat
 start cmd /k Call .Internet.bat
+start cmd /k Call .Reg.bat
 pushd %~dp0
 start /wait "" cmd /c cscript .CD-ROM.vbs
 start cmd /k Call .Matrix.bat
